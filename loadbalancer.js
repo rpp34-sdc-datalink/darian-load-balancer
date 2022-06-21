4785c87494941f81d7952ba26e744feb891ecbc7
Darian Hogue <hogued96@gmail.com>
2022-06-21 10:56:29 -0400

    adjusted loadbalancer

--- loadbalancer.js
+++ loadbalancer.js
@@ -10,43 +10,22 @@ const serverURLs = {
   '1': 'http://44.203.171.122:8024',
   '2': 'http://44.202.58.18:8024'
 }
-let currentServer = '2';
-let serverReqCount = 0;
+let currentServer = '1';
 
 const serverSwitch = function(req, res, next) {
-  serverReqCount++;
-  if (serverReqCount >= 100) {
-    serverReqCount = 0;
-    if (currentServer === '1') {
-      currentServer = '2';
-    } else {
-      currentServer = '1';
-    }
+  if (currentServer === '1') {
+    currentServer = '2';
+  } else {
+    currentServer = '1';
   }
   next()
 }
-let serverURL = serverURLs[currentServer];
-
-app.all('/reviews', (req, res) => {
-  serverReqCount++;
-  if (serverReqCount >= 100) {
-    serverReqCount = 0;
-    if (currentServer === '1') {
-      currentServer = '2';
-    } else {
-      currentServer = '1';
-    }
-  }
-
-    res.redirect(serverURL)
-
-})
-
 app.get('/loaderio-2e0bc4c775d5023276a39b3cf12bf9d6.txt', (req, res) => {
   res.sendFile(path.join(__dirname,'/loaderio-2e0bc4c775d5023276a39b3cf12bf9d6.txt'));
 })
-// app.use(serverSwitch)
-// app.use('/reviews', createProxyMiddleware({ target: serverURL, changeOrigin: true }));
+
+app.use(serverSwitch)
+app.use('/reviews', createProxyMiddleware({ target: serverURLs[currentServer], changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
